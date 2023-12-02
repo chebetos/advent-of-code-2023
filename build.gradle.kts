b7d38f4783d827778397b0c5d2d653a64506d378
Cesar Augusto Delgado Rodriguez <chebetos@gmail.com>
2023-12-03 00:11:24 +0100

    Updated project skeleton and gradle build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,24 @@
 plugins {
     kotlin("jvm") version "1.9.21"
+    application
 }
 
-sourceSets {
-    main {
-        kotlin.srcDir("src")
-    }
+group = "es.chebetos.advent2023"
+version = "1.0-SNAPSHOT"
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+kotlin {
+    jvmToolchain(21)
 }
 
 tasks {
+    test {
+        useJUnitPlatform()
+    }
     wrapper {
         gradleVersion = "8.5"
     }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,6 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
+}
 rootProject.name = "advent-of-code-2023"
 
 dependencyResolutionManagement {
