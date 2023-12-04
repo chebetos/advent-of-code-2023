f7d728004a6d61815a0190add8d805631821474b
Cesar Augusto Delgado Rodriguez <chebetos@gmail.com>
2023-12-04 07:54:53 +0100

    Initial setup for day02

---
+++ src/main/kotlin/day02/Day02.kt
@@ -0,0 +1,22 @@
+package day02
+
+import println
+
+object Day02 {
+    fun part1(lines: List<String>): Int {
+        return 0
+    }
+
+    fun part2(lines: List<String>): Int {
+        return 0
+    }
+
+    fun main() {
+        val day02Input = Utils.readInput("day02_input.txt")
+        val part1Result = Day02.part1(day02Input)
+        part1Result.println()
+
+        val part2Result = Day02.part2(day02Input)
+        part2Result.println()
+    }
+}
---
+++ src/test/kotlin/day02/Day02Test.kt
@@ -0,0 +1,18 @@
+package day02
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Day02Test {
+
+    private val day02 = Day02
+
+    @Test
+    fun part1() {
+        assertEquals(0, day02.part1(listOf()))
+    }
+
+    @Test
+    fun part2() {
+    }
+}
