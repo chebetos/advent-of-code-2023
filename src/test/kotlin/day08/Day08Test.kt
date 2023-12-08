54ae24cf71956cef70924e10eda9da9cf48df10b
Cesar Augusto Delgado Rodriguez <chebetos@gmail.com>
2023-12-08 16:41:09 +0100

    Added Day 08 skeleton

---
+++ src/test/kotlin/day08/Day08Test.kt
@@ -0,0 +1,16 @@
+package day08
+
+import org.junit.jupiter.api.Assertions.*
+
+import org.junit.jupiter.api.Test
+
+class Day08Test {
+
+    @Test
+    fun part1() {
+    }
+
+    @Test
+    fun part2() {
+    }
+}
