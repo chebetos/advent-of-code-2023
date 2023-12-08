2a528c914ebfad7f8e9a39616492eec815b3263b
Cesar Augusto Delgado Rodriguez <chebetos@gmail.com>
2023-12-08 17:50:50 +0100

    Implemented Day 08 part 1

--- src/main/kotlin/day08/Day08.kt
+++ src/main/kotlin/day08/Day08.kt
@@ -13,10 +13,58 @@ fun main() {
 
 object Day08 {
     fun part1(lines: List<String>): Int {
-        return lines.size
+        val network = Network.fromString(lines)
+        println("Network: $network")
+        return network.navigate()
     }
 
     fun part2(lines: List<String>): Int {
         return lines.size
+    }
+}
+
+data class Network(
+    val leftRightInstructions: String,
+    val network: Map<String, Pair<String, String>>
+) {
+    companion object {
+        fun fromString(lines: List<String>): Network {
+            val leftRightInstructions = lines[0]
+            val network = LinkedHashMap<String, Pair<String, String>>()
+            lines.drop(2).forEach {
+                val lineArray = it.split('=').map { s -> s.trim() }
+                val key = lineArray[0]
+                val values = lineArray[1]
+                    .removeSurrounding("(", ")")
+                    .split(',')
+                    .map{ it.trim() }
+                val value = Pair(values[0], values[1])
+                network.put(key, value)
+            }
+
+            return Network(leftRightInstructions, network)
+        }
+    }
+
+    fun navigate(): Int {
+        var counter = 0
+        var currentInstruction = 0
+        var currentPos = "AAA"
+        while (!"ZZZ".equals(currentPos)) {
+            counter++
+
+            val currentPositionNetworkElement = network.getValue(currentPos)
+            val whereToGo = leftRightInstructions[currentInstruction]
+            when (whereToGo) {
+                'L' -> currentPos = currentPositionNetworkElement.first
+                'R' -> currentPos = currentPositionNetworkElement.second
+                else -> println("Invalid instruction: $whereToGo")
+            }
+            currentInstruction++
+            if (leftRightInstructions.length == currentInstruction)  {
+                currentInstruction = 0
+            }
+        }
+        return counter
     }
 }
--- src/test/kotlin/day08/Day08Test.kt
+++ src/test/kotlin/day08/Day08Test.kt
@@ -1,13 +1,19 @@
 package day08
 
-import org.junit.jupiter.api.Assertions.*
-
 import org.junit.jupiter.api.Test
 
 class Day08Test {
 
     @Test
     fun part1() {
+        val testInput = Utils.readInput("Day08_part1_text.input")
+        val networkA = Day08.part1(testInput)
+        kotlin.test.assertEquals(2, networkA)
+
+        val testInputB = Utils.readInput("Day08_part1b_text.input")
+        val networkB = Day08.part1(testInputB)
+        kotlin.test.assertEquals(6, networkB)
+
     }
 
     @Test
